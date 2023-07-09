3da1675287525d9cf9680917904599253ee06af8
AndreaLinza <andrealinza@gmail.com>
2023-07-09 17:12:21 +0200

    snack 3 incompleted

---
+++ Snack 3(bonus)/script.js
@@ -0,0 +1,34 @@
+let nArray = 0
+
+nArray = parseInt(prompt(("Inserisci un numero")))
+
+do{
+
+    alert("Inserisci un numero valido!!")
+    nArray = parseInt(prompt(("Inserisci un numero")))
+
+}while (nArray < 0 || isNaN(nArray) || nArray == "");
+
+
+
+
+    /*else if (nArray > 0) {
+
+        let arrayUser = []
+        console.log(nArray)
+
+        while (nArray = 0) {
+
+            a
+            nArray--
+            console.log(nArray)
+            console.log(arrayUser)
+        }
+    }
+
+    else {
+        break
+    }
+}*/
+
+
