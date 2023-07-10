d0a853caf5ba0b168ba115733e4d33debf5a0f1d
AndreaLinza <andrealinza@gmail.com>
2023-07-10 17:20:37 +0200

    bonus fixed

--- Snack 3(bonus)/script.js
+++ Snack 3(bonus)/script.js
@@ -1,34 +1,34 @@
 let nArray = 0
 const result = document.querySelector("[id=result]")
 
-nArray = parseInt(prompt(("Inserisci un numero")))
 
 do {
+    nArray = parseInt(prompt(("Inserisci un numero")))
     if (nArray < 0 || isNaN(nArray) || nArray == "") {
 
         alert("Inserisci un numero valido!!")
         nArray = parseInt(prompt(("Inserisci un numero")))
         
-        if (nArray > 0) {
-            const numInput = nArray
-            for( let i=0; i<numInput; i++){
+    }
+    if (nArray > 0) {
+    const numInput = nArray
+        for( let i=0; i<numInput; i++){
+            
+            let randomArray = []
+            const div = document.createElement("div")
+            
+            for (let i = 0; i < 10; i++) {
                 
-                let randomArray = []
-                const div = document.createElement("div")
+                const randomNumber = Math.floor(Math.random() * 100 + 1)
+                randomArray.push(randomNumber)
                 
-                for (let i = 0; i < 10; i++) {
-                    
-                    const randomNumber = Math.floor(Math.random() * 100 + 1)
-                    randomArray.push(randomNumber)
-                    
-                }
-                
-                div.innerHTML = (randomArray)
-                result.append(div)
             }
             
-            
+            div.innerHTML = (randomArray)
+            result.append(div)
         }
+        
+        
     }
 
 
